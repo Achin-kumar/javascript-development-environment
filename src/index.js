64de57c9a7a76ed117fde2740478fe179f91e655
Achin-kumar <achin_kr@hotmail.com>
2018-06-25 14:54:14 +0530

    Integrated the json using api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import './index.css';
 
-// import {getUsers, deleteUser} from './api/userApi';
+import { getUsers } from './api/userApi';
 
 // Populate table of users via API call.
 getUsers().then(result => {
   let usersBody = "";
 
   result.forEach(user => {
-    usersBody+= `<tr>
+    usersBody += `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-      <td>${user.id}</td>
+      <td>${user.id}</td> 
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
       <td>${user.email}</td>
       </tr>`
   });
+  console.log("Result is here");
 
   global.document.getElementById('users').innerHTML = usersBody;
 });
